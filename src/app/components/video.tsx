a6bd0d44072dec07cc9d1b9374050aa6b72ffa84
ANIL KUMAR SINGH KUSHWAHA <90541938+anil9450@users.noreply.github.com>
2024-06-29 11:31:54 +0530

    28-06-2024

--- src/app/components/video.tsx
+++ src/app/components/video.tsx
@@ -8,9 +8,8 @@ const Video = () => {
         src="https://www.youtube.com/embed/imxdoV9Qsbg"
         width="100%"
         title="YouTube video player"
-        frameborder="0"
         allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share"
-        allowfullscreen
+        allowFullScreen
       ></iframe>
     </div>
   );
